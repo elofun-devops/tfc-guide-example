4781e09801e27006b826e2e609e32a48fce86c06
khanh <k3nkukhanh@gmail.com>
2024-09-25 10:04:58 +0000

    edit

--- main.tf
+++ main.tf
@@ -1,31 +1,13 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-provider "aws" {
-  region = var.region
+provider "google" {
+  project     = "demo"
+  region      = "us-central1"
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
+resource "google_dns_managed_zone" "public-zone" {
+  name        = "hocdevops"
+  dns_name    = "abc.hocdevops.me."
+  description = "Demo DNS zone"
+  labels = {
+    foo = "demo"
   }
 }
+
